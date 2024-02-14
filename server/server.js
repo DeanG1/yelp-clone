9f8fe02096e16115f055cee3953b587035419559
Dido2002 <didoguzelev@gmail.com>
2024-02-14 10:49:28 +0200

    setting_server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.listen(3005, () => {
-    console.log("Server is running on port 3005");
+app.use((req, res, next) => {
+    console.log("yeah middle ware");
+    next();
 })
+
+
+//Get all Restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    res.status(200).json({
+        status:"success",
+        data:{
+            restaurant:["mcdonalds","wendys"]
+        },
+    });
+});
+
+//Get a Restaurant
+app.get("/api/v1/restaurants/:id", (req, res)=>{
+    console.log(req.params);
+});
+
+//Create a Restaurant
+app.post("/api/v1/restaurants", (req,res)=>{
+    console.log(req);
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
