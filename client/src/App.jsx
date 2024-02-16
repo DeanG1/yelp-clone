73718680b355e2c17c0cbe66a86e8f2ea2136179
Dido2002 <didoguzelev@gmail.com>
2024-02-16 21:30:31 +0200

    QuickFixes+RoutesSeting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import Home from "./routes/Home";
+import UpdataPage from "./routes/UpdatePage";
 
 const App = () => {
-    return(
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdataPage} />
+          <Route
+            exact
+            path="/restaurants/:id"
+            component={RestaurantDetailPage}
+          />
+        </BrowserRouter>
+      </Router>
+    </div>
+  );
+};
 export default App;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantDetailPage() {
+  return (
+    <div>RestaurantdetailPage</div>
+  )
+}
+
+export default RestaurantDetailPage
