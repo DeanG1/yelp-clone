34d537c9978aa83ff24daa0a89a3abbd1ee2e12e
Dido2002 <didoguzelev@gmail.com>
2024-02-17 20:04:54 +0200

    CreateRestaurant+RenderIt

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { useContext } from "react";
+import { RestaurantsContext } from "../context/RestaurantContext";
 const AddRestaurant = () => {
-  return (
-    <div className='mb-4'>
-        <form action="">
-            <div className='form-group'>
-                <div className="col">
-                    <input type="text" className="form-control" placeholder='Name' />
-                </div>
-                <div className='col'>
-                    <input type="text" className="form-control" placeholder='Location' />
-                </div>
-                <div className="col">
-                    <select className='custom-select mr-sm-2'>
-                        <option disabled>
-                            Price Range
-                        </option>
-                        <option value="1">$</option>
-                        <option value="2">$$</option>
-                        <option value="3">$$$</option>
-                        <option value="4">$$$$</option>
-                        <option value="5">$$$$$</option>
-                    </select>
-                </div>
-                <button className="btn btn-primary">Add</button>
-            </div>
-        </form>
-    </div>
-  )
-}
+    const {addRestaurants} = useContext(RestaurantsContext)
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
 
-export default AddRestaurant
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try{
+            const response = await RestaurantFinder.post("/",{
+                name,
+                location,
+                price_range: priceRange
+            });
+            addRestaurants(response.data.data.restaurant);
+            console.log(response)
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
+  return (
+    <div className="mb-4">
+      <form action="">
+        <div className="form-group">
+          <div className="col">
+            <input value={name} onChange={e => setName(e.target.value)} type="text" className="form-control" placeholder="Name" />
+          </div>
+          <div className="col">
+            <input
+              value={location}
+              onChange={e => setLocation(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Location"
+            />
+          </div>
+          <div className="col">
+            <select value={priceRange} onChange={e => setPriceRange(e.target.value)} className="custom-select mr-sm-2">
+              <option disabled>Price Range</option>
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+              <option value="5">$$$$$</option>
+            </select>
+          </div>
+          <button type="submit" onClick={handleSubmit} className="btn btn-primary">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddRestaurant;
--- client/src/context/RestaurantContext.js
+++ client/src/context/RestaurantContext.js
@@ -2,12 +2,16 @@ import React,{useState, createContext} from "react";
 
 export const RestaurantsContext = createContext();
 
-export const RestaurantsContextProvider = props => {
+export const RestaurantsContextProvider = (props) => {
 
     const [restaurants, setRestaurants] = useState([]);
 
+    const addRestaurants = (restaurant) => {
+        setRestaurants([...restaurants, restaurant])
+    }
+
     return(
-        <RestaurantsContext.Provider value={{restaurants, setRestaurants}}>
+        <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants}}>
             {props.children}
         </RestaurantsContext.Provider>
     )
