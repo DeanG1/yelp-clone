036491213de73e602d75f1300a6e59dc0161e6c3
Dido2002 <didoguzelev@gmail.com>
2024-02-17 10:28:23 +0200

    RestaurantsFetchingFromBackendAndRendering

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,57 +1,57 @@
-import React,{useEffect} from 'react'
-import RestaurantFinder from '../apis/RestaurantFinder'
-import { useContext } from 'react'
-import { RestaurantsContext } from '../context/RestaurantContext'
+import React, { useEffect } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { useContext } from "react";
+import { RestaurantsContext } from "../context/RestaurantContext";
 
 const RestaurantList = (props) => {
-    const {restaurants,setRestaurants} = useContext(RestaurantsContext);
+    const { restaurants, setRestaurants } = useContext(RestaurantsContext);
     useEffect(() => {
-        const fetchData = async () =>{
-            try{
-                const response = await RestaurantFinder.get("/")
-                setRestaurants(response.data.data.restaurants);
-            }
-            catch(err){
-                console.log(err);
-            }
-        };
-        fetchData();
-    },[])
+      const fetchData = async () => {
+        try {
+          const response = await RestaurantFinder.get("/");
+          console.log(response.data.data);
+          setRestaurants(response.data.data.restaurant);
+        } catch (err) {}
+      };
+  
+      fetchData();
+    }, []);
 
   return (
-    <div className='list-group'>
-        <table className='table table-hover table-dark'>
-            <thead>
-                <tr className='bg-primary'>
-                    <th scope="col">Restaurant</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Price Range</th>
-                    <th scope="col">Rating</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
+    <div className="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants &&
+            restaurants.map((restaurant) => {
+              return (
+                <tr key={restaurant.id}>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{"$".repeat(restaurant.price_range)}</td>
+                  <td>Reviews</td>
+                  <td>
+                    <button className="btn btn-warning">Update</button>
+                  </td>
+                  <td>
+                    <button className="btn btn-danger">Delete</button>
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>mcDonald</td>
-                    <td>New York</td>
-                    <td>$$$</td>
-                    <td>Rating</td>
-                    <td><button className='btn btn-warning'>Update</button></td> 
-                    <td><button className='btn btn-danger'>Delete</button></td>
-                </tr>
-                <tr>
-                    <td>mcDonald</td>
-                    <td>New York</td>
-                    <td>$$$</td>
-                    <td>Rating</td>
-                    <td><button className='btn btn-warning'>Update</button></td> 
-                    <td><button className='btn btn-danger'>Delete</button></td>
-                </tr>
-            </tbody>
-        </table>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantList
+export default RestaurantList;
