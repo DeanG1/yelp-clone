a6b4e90a510e81ddec051053c4a3c7d110afbf67
Dido2002 <didoguzelev@gmail.com>
2024-02-18 11:25:59 +0200

    RendingRestairantDetailPage

--- client/src/context/RestaurantContext.js
+++ client/src/context/RestaurantContext.js
@@ -1,19 +1,25 @@
-import React,{useState, createContext} from "react";
+import React, { useState, createContext } from "react";
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
-    const [restaurants, setRestaurants] = useState([]);
-
-    const addRestaurants = (restaurant) => {
-        setRestaurants([...restaurants, restaurant])
-    }
-    
-
-    return(
-        <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants}}>
-            {props.children}
-        </RestaurantsContext.Provider>
-    )
-}
+  const addRestaurants = (restaurant) => {
+    setRestaurants([...restaurants, restaurant]);
+  };
+  return (
+    <RestaurantsContext.Provider
+      value={{
+        restaurants,
+        setRestaurants,
+        addRestaurants,
+        selectedRestaurant,
+        setSelectedRestaurant,
+      }}
+    >
+      {props.children}
+    </RestaurantsContext.Provider>
+  );
+};
