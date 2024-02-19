984ac21ed17bcd6cb86dbc53b834fd260b81274b
Dido2002 <didoguzelev@gmail.com>
2024-02-19 08:46:29 +0200

    Functionality&RenderingReviews

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -4,6 +4,7 @@ import { RestaurantsContext } from "../context/RestaurantContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import StarRating from "../components/StarRating";
 import Reviews from "../components/Reviews";
+import AddReview from "../components/AddReview";
 
 function RestaurantDetailPage() {
   const { id } = useParams();
@@ -14,7 +15,7 @@ function RestaurantDetailPage() {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data);
       } catch (err) {
         console.log(err);
       }
@@ -24,9 +25,11 @@ function RestaurantDetailPage() {
 
   return <div>{selectedRestaurant && (
     <>
+    <h1 className="text-center display-1">{selectedRestaurant.restaurant.name}</h1>
     <div className="mt-3">
-      <Reviews/>
+      <Reviews reviews={selectedRestaurant.reviews}/>
     </div> 
+    <AddReview/>
     </>
   )}</div>;
 };
